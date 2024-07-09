761966ba1606a3ff6524910639ca3eb1551c0bec
DRAVEN1022 <131804571+DRAVEN1022@users.noreply.github.com>
2024-07-09 13:19:14 +0530

    spam.py

--- spam.py
+++ spam.py
@@ -2,7 +2,7 @@ import requests
 import json
 from colorama import Fore
 
-def send_message(webhookurl, content, times):
+def send_message(webhook_url, content, times):
     headers = {
         'Content-Type': 'application/json',
     }
@@ -14,14 +14,15 @@ def send_message(webhookurl, content, times):
     for _ in range(times):
         response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
 
-        if response.statuscode == 204:
-            print(f"{Fore.GREEN}[ .gg/nuker-x  ] {  + 1 } Message  {message_content}  sent successfully!")
+        if response.status_code == 204:
+            print(f"{Fore.GREEN}[ .gg/nuker-x  ] { _ + 1 } Message  {message_content}  sent successfully!")
         else:
             print(f"{Fore.RED}[ .gg/nuker-x ] Failed to send message. Status code: {response.status_code}")
 
-if name == "main":
+if __name__ == "__main__":
     webhook_url = input("ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴡᴇʙʜᴏᴏᴋ ʟɪɴᴋ : ")
     message_content = input("ᴇɴᴛᴇʀ ᴍᴇꜱꜱᴀɢᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴇɴᴅ : ")
     message_count = int(input("ᴇɴᴛᴇʀ ɴᴜᴍʙᴇʀ ᴏꜰ ᴍᴇꜱꜱᴀɢᴇ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ꜱᴇɴᴅ : "))
 
     send_message(webhook_url, message_content, message_count)
+    
